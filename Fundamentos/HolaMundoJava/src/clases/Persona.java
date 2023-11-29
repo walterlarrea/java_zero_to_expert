dbdbdf91b4554a3f8561c0f5e65f9f1ae501fa85
Walter Larrea <walterlarrea@live.com>
2023-11-29 19:28:10 -0300

    Section 8 - fundamentos clases

---
+++ Fundamentos/HolaMundoJava/src/clases/Persona.java
@@ -0,0 +1,11 @@
+package clases;
+
+public class Persona {
+  String nombre;
+  String apellido;
+
+  public void desplegarInformacion() {
+    System.out.println("Nombre: " + nombre);
+    System.out.println("Apellido: " + apellido);
+  }
+}
